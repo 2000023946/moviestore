f8b2f8d22925423edb162d6adddcb06289c8f599
mo <mo@gmail.com>
2025-09-03 15:00:40 -0400

    first version

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render #type: ignore
+
+def index(request):
+    template_data = {}
+    template_data['title'] = 'Movies Store'
+    return render(request, 'home/index.html', {
+        'template_data': template_data})
+
+
+def about(request):
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request,
+        'home/about.html',
+        {'template_data': template_data})
+
