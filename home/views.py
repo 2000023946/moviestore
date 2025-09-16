4a48a66106cd2b1ea043b20bd4da7e9d1c0bee2e
mo <mo@gmail.com>
2025-09-16 14:22:59 -0400

    commit

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render #type: ignore
+from cart.models import Order
+from django.db.models import Sum
+
 
 def index(request):
     template_data = {}
@@ -14,4 +17,26 @@ def about(request):
     return render(request,
         'home/about.html',
         {'template_data': template_data})
+
+def subs(request):
+    template_data = {}
+    template_data['title'] = 'About'
+    subs = get_total_spent(request.user)
+    if subs < 15:
+        template_data['subscription'] = 'Basic'
+    elif subs < 30:
+        template_data['subscription'] = 'Medium'
+    else: 
+        template_data['subscription'] = 'Premium'
+
+    return render(request,
+        'home/subs.html',
+        {'template_data': template_data})
+
+def get_total_spent(user):
+    return (
+        Order.objects
+        .filter(user=user)
+        .aggregate(total_spent=Sum('total'))
+    )['total_spent'] or 0
 
